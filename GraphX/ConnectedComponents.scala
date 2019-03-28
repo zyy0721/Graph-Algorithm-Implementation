bec021faf0890a6779e8608836908e3f95015bf2
zyy0721 <zhangyy_0721@163.com>
2019-03-28 20:02:40 +0800

    Impl Connected Components alg in GraphX

---
+++ GraphX/ConnectedComponents.scala
@@ -0,0 +1,26 @@
+import scala.reflect.ClassTag
+import org.apache.spark.graphx._
+
+/** Computes Connected components membership of each vertex and return a graph with the vertex
+* value containing the lowest vertex id in the connected component containing that vertex.
+*/
+object ConnectedComponents {
+
+  def run[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED]): Graph[VertexId, ED] = {
+    val ccGraph = graph.mapVertices { case (vid, _) => vid }
+    def sendMessage(edge: EdgeTriplet[VertexId, ED]): Iterator[(VertexId, VertexId)] = {
+      if (edge.srcAttr < edge.dstAttr) {
+        Iterator((edge.dstId, edge.srcAttr))
+      } else if (edge.srcAttr > edge.dstAttr) {
+        Iterator((edge.srcId, edge.dstAttr))
+      } else {
+        Iterator.empty
+      }
+    }
+    val initialMessage = Long.MaxValue
+    Pregel(ccGraph, initialMessage, activeDirection = EdgeDirection.Either)(
+      vprog = (id, attr, msg) => math.min(attr, msg),
+      sendMsg = sendMessage,
+      mergeMsg = (a, b) => math.min(a, b))
+  } 
+}
